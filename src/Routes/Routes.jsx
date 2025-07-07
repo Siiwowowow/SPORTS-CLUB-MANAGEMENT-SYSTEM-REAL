7c78a9845e82bb5b92d9733c7dd1c817c48b4940
Siiwowowow <siaa32290@gmail.com>
2025-07-07 22:45:10 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router";
+ export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello World</div>,
+    },
+  ]);
